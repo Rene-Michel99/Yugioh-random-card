2e5cc31f62bc3ffdd6777a8cca7ade53a9a6ddea
Rene-Michel99 <renemichel856@gmail.com>
2021-05-16 00:27:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let imgs = [];
+for (let i = 0; i < 37; i++){
+    imgs.push("card"+i.toString()+".jpg");
+}
+console.log(imgs);
+function random_img(){
+    const index = Math.floor(Math.random() * imgs.length)+1;
+
+    const box = document.getElementById("card_holder");
+    box.innerHTML = "";
+
+    const img = document.createElement("img");
+    img.src = "./src/card"+index.toString()+".jpg";
+    img.width = "300";
+    img.height = "300";
+    img.className = "card";
+
+    box.appendChild(img);
+    console.log("OK");
+}
